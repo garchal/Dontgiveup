f9e3fddd84e6b151f7ca133460fbc1f152eb4bbe
garchal <99027994+garchal@users.noreply.github.com>
2025-04-17 10:33:55 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdfcf1LBcR0WkMQcGd1DLJ6tF3nxPZvWI",
+  authDomain: "poetic-notebook.firebaseapp.com",
+  projectId: "poetic-notebook",
+  storageBucket: "poetic-notebook.appspot.com",
+  messagingSenderId: "568335343667",
+  appId: "1:568335343667:web:0a8571272824ad857b0346"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
